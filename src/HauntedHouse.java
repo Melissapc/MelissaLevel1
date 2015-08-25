f3914119c4dc741d4485e4aed08fc68962030f53
Melissa <Guest@wts8.wts.local>
2015-08-24 18:24:53 -0700

    Code 8/24/15
    fun day!

---
+++ src/HauntedHouse.java
@@ -0,0 +1,27 @@
+public class HauntedHouse {
+	public static void main(String[] args) {
+	HauntedHouse h = new HauntedHouse("SpookyZone", 105);
+		h.doScaryStuff(1000000, "Boo!");
+		h.isSpooky();
+	}
+	int skeletons;
+	String name;
+
+	public HauntedHouse(String name, int skeletons) {
+		this.name = name;
+		this.skeletons = skeletons;
+	}
+
+	void doScaryStuff(int pumpkins, String scaryPhrase) {
+		System.out.println(pumpkins + " pumpkins say" + scaryPhrase);
+	}
+
+	boolean isSpooky() {
+		// Haunted houses are spooky!
+		return true;
+	}
+
+
+		
+	
+}
