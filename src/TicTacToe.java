a85f4aeaffdc25c40ee7a14c982f17da015d8964
Melissa <Guest@wts8.wts.local>
2015-09-28 18:27:05 -0700

    Tic Tac toe
    really new to me

---
+++ src/TicTacToe.java
@@ -0,0 +1,32 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TicTacToe {
+	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe();
+
+	}
+
+	TicTacToe() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		panel.setVisible(true);
+
+		frame.add(panel);
+		b1.addActionListener(this);
+
+		JButton b1 = new JButton();
+
+		JButton b2 = new JButton();
+		JButton b3 = new JButton();
+		JButton b4 = new JButton();
+		JButton b5 = new JButton();
+		JButton b6 = new JButton();
+		JButton b7 = new JButton();
+		JButton b8 = new JButton();
+		JButton b9 = new JButton();
+	}
+
+}
