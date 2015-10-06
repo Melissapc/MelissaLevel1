9e1fcbbb61bddbbd1dc707238b80c44cbfc834f7
Melissa <Guest@wts8.wts.local>
2015-10-05 18:30:43 -0700

    practicing
    code 10.5.15

--- src/Sherlissa.java
+++ src/Sherlissa.java
@@ -1,7 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Sherlissa {
+public class Sherlissa implements ActionListener {
+	JButton mel = new JButton();
+	JButton sher = new JButton();
+
 	public static void main(String[] args) {
 		Sherlissa me = new Sherlissa();
 
@@ -10,8 +17,30 @@ public class Sherlissa {
 	Sherlissa() {
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
+		frame.setSize(250, 250);
 		JPanel panel = new JPanel();
-		panel.setVisible(true);
 		frame.add(panel);
+
+		mel.addActionListener(this);
+
+		sher.addActionListener(this);
+		mel.setVisible(true);
+		mel.setText("Melissa");
+
+		sher.setVisible(true);
+		sher.setText("sherlin");
+		panel.add(mel);
+		panel.add(sher);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(mel)) {
+			System.out.println("melissa rocks!");
+
+		} else if (e.getSource().equals(sher)) {
+			System.out.println("sherlin rocks!");
+		}
 	}
 }
