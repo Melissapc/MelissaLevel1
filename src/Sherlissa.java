1b63448e061d25c9f6899efc71e76d63eeb758da
Melissa <Guest@wts8.wts.local>
2015-10-05 17:16:50 -0700

    fixed class path for AdvancedPlayer

---
+++ src/Sherlissa.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Sherlissa {
+	public static void main(String[] args) {
+		Sherlissa me = new Sherlissa();
+
+	}
+
+	Sherlissa() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		panel.setVisible(true);
+		frame.add(panel);
+	}
+}
